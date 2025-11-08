e1e3cb1656ac7bbffa00168785a9f185ee86e780
Lrigobert <ldardaorigobert78@gmail.com>
2025-11-08 19:05:21 +0000

    feat: Mon 2eme  de hook commit test eslint

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,6 @@ app.get("/", (req: Request, res: Response) => {
 
 
 // Démarrage du serveur
-app.listen(PORT, () => {
+app.listen  (PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
-});
+;
