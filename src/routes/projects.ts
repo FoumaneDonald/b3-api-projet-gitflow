07c347e35203ec83bdd7c1c986c43034ec413b47
FoumaneDonald <foumaned9@gmail.com>
2025-11-10 00:37:27 +0100

    Revert "Merge branch 'feature/grade-project' into develop"
    This reverts commit 1448b07bfe58cf693722a701af80493e3aca92e7, reversing
    changes made to eb24c125c0a05482f8dfcd430703cdc56bd52365.

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -4,6 +4,5 @@ const projectController = require("../controllers/projectController");
 
 router.get("/", projectController.projectsHomes);
 // router.get("/course", projectController.register);
-router.put("/:id/grade", projectController.updateGrade);
 
 module.exports = router;
