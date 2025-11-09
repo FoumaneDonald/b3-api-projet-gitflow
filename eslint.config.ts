76c14cbd0a4d6b9523876c4b5e86b7485f04e35e
FoumaneDonald <foumaned9@gmail.com>
2025-11-09 11:49:41 +0100

    feat: block commit on bad formatted code

--- eslint.config.ts
+++ eslint.config.ts
@@ -27,7 +27,7 @@ export default defineConfig([
         { argsIgnorePattern: "^_" },
       ],
       // Warn (but don't block) when using "any"
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-require-imports": "off",
     },
   },
