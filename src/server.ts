86bc7cf9bdf509a368506b67379ca8653bf3e496
yeillenr <roseorange76@gmail.com>
2025-11-10 02:59:00 +0100

    feat:affichage de la liste des projets

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,7 @@
-const app = require("./app");
-const http = require("http");
+import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-const server = http.createServer(app);
-
-process
-  .on("uncaughtException", (err) => {
-    console.error(err.message, "Uncaught Exception thrown");
-    server.close();
-  })
-  .on("unhandledRejection", (reason) => {
-    console.error(reason, "Unhandled Rejection at Promise");
-  });
-
-const main = async () => {
-  await server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-};
-
-main();
+app.listen(PORT, () => {
+  console.log(` Server is running on http://localhost:${PORT}`);
+});
